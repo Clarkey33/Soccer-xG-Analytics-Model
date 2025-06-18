205f8ab2853d0674c7b0e210bd34e5b894b0050b
Chadrick Clarke <chadrickclarkea@gmail.com>
2025-06-18 02:51:31 -0400

    "updates"

---
+++ src/ModelDevelopment.py
@@ -0,0 +1,24 @@
+#library required
+from sklearn.metrics import roc_auc_score, classification_report, confusion_matrix
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import Pipeline
+
+
+class Pipeline 
+
+# function model validation with cross val
+
+#function setup preprocess pipeline
+
+#function setup  pipeline (preprocess + moddel)
+
+#function train pipeline and predict with x_train
+
+#function predict with X-test
+
+#function evaluation metrics; usable on both train and test scenarios without leakage
+
+#hyper parameter tunin
+
